bf28d1437c9e65fdc7e3451203d3c7b927bafa9a
M. Resul Yılmaz <78299014+mryilmz@users.noreply.github.com>
2025-02-10 18:19:22 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import { initializeApp } from "firebase/app";
+     import { getAuth } from "firebase/auth";
+     import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyARbcAtLvuQLn3MuTlIakDs5Jm-MYW-YoU",
-    authDomain: "libramaster-d22da.firebaseapp.com",
-    projectId: "libramaster-d22da",
-    storageBucket: "libramaster-d22da.firebasestorage.app",
-    messagingSenderId: "370591319637",
-    appId: "1:370591319637:web:ea1712f031101dd017b6dc",
-    measurementId: "G-TG45HDPYD1"
-  };
+     const firebaseConfig = {
+       apiKey: "AIzaSyARbcAtLvuQLn3MuTlIakDs5Jm-MYW-YoU",
+       authDomain: "libramaster-d22da.firebaseapp.com",
+       projectId: "libramaster-d22da",
+       storageBucket: "libramaster-d22da.firebasestorage.app",
+       messagingSenderId: "370591319637",
+       appId: "1:370591319637:web:ea1712f031101dd017b6dc"
+     };
 
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+     const app = initializeApp(firebaseConfig);
+     export const auth = getAuth(app);
+     export const db = getFirestore(app);
